916632ac6ddd2dcd4442ed2f6f852f550f9e0b75
Abhishek khati chhetri <abhichhetri@outlook.com>
2024-06-04 01:57:41 +0300

    new app

---
+++ part1.1/course_Information/src/App.jsx
@@ -0,0 +1,8 @@
+
+const app = () =>{
+  const course = "Half Stack application development"
+  const part1 = "Fundaments of React"
+  const exercise1 = 10
+  const part2 = "Using props to pass data"
+  const 
+}
