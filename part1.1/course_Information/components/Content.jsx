08b235dd2d01ebe864d2a715917e229a8e0a0a36
Abhishek khati chhetri <abhichhetri@outlook.com>
2024-06-04 02:19:31 +0300

    prop passed and tested for header

---
+++ part1.1/course_Information/components/Content.jsx
@@ -0,0 +1,13 @@
+const Content = () =>{
+    const part1 = "Fundamentals of React"
+    const exercise1 = 10
+    const part2 = "Using props to pass data"
+    const exercise2 = 7
+    const part3 = "State of a component"
+    const exercise3 = 14
+    return(
+    <div>
+        {}
+    </div>
+    )
+}
