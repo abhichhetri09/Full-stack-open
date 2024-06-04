c3b8d9b91747f09b984c7e2d6d8bd800b9e56b22
Abhishek khati chhetri <abhichhetri@outlook.com>
2024-06-04 16:54:51 +0300

    part component added

--- part1.1/course_Information/components/Content.jsx
+++ part1.1/course_Information/components/Content.jsx
@@ -1,16 +1,13 @@
-const Content = ({part1, exercise1,part2,exercise2, part3,exercise3}) =>{
+import Part from './Part';
 
+const Content = ({part1, exercise1,part2,exercise2, part3,exercise3}) =>{
     return(
     <div>
-        <p>
-            {part1}{exercise1}
-        </p>
-        <p>
-            {part2}{exercise2}
-        </p>
-        <p>
-            {part3}{exercise3}
-        </p>
+            <Part name={part1} exercise={exercise1} />
+            <Part name={part2} exercise={exercise2} />
+            <Part name= {part3} exercise={exercise3} />
+
+       
     </div>
     )
 }
