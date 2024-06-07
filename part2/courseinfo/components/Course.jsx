8aef0fc7ab42d5e77f06bb0b1b1d01d1a5d2263a
Abhishek khati chhetri <abhichhetri@outlook.com>
2024-06-07 23:48:51 +0300

    2.5 done

--- part2/courseinfo/components/Course.jsx
+++ part2/courseinfo/components/Course.jsx
@@ -2,12 +2,18 @@ import React from "react";
 import Header from "./Header";
 import Content from "./Content";
 import Part from "./Part";
+import Total from "./Total";
 
 const Course = ({ course }) => {
   return (
     <div>
-      <Header course={course} />
-      <Content parts={course.parts} />
+      {course.map((course) => (
+        <div key={course.id}>
+          <Header course={course} />
+          <Content parts={course.parts} />
+          <Total parts={course.parts} />
+        </div>
+      ))}
     </div>
   );
 };
--- part2/courseinfo/components/Total.jsx
+++ part2/courseinfo/components/Total.jsx
@@ -1,6 +1,6 @@
 const Total = ({ parts }) => {
   const totalExercises = parts.reduce((sum, part) => sum + part.exercises, 0);
-  return <p>Number of exercises: {totalExercises}</p>;
+  return <p>total of :{totalExercises} exercises</p>;
 };
 
 export default Total;
