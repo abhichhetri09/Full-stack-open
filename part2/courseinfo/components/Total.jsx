d74915e637dbd778a3ce49f1e1ade68d926f44eb
Abhishek khati chhetri <abhichhetri@outlook.com>
2024-06-08 00:24:44 +0300

    Update Total.jsx

--- part2/courseinfo/components/Total.jsx
+++ part2/courseinfo/components/Total.jsx
@@ -1,6 +1,10 @@
 const Total = ({ parts }) => {
   const totalExercises = parts.reduce((sum, part) => sum + part.exercises, 0);
-  return <p>total of :{totalExercises} exercises</p>;
+  return (
+    <p>
+      <b>total of :{totalExercises} exercises</b>
+    </p>
+  );
 };
 
 export default Total;
