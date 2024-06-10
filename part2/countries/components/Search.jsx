21127fdccecb71f84976bb5ba3a702f88b31e3cb
Abhishek khati chhetri <abhichhetri@outlook.com>
2024-06-11 02:02:08 +0300

    search component created

--- part2/countries/components/Search.jsx
+++ part2/countries/components/Search.jsx
@@ -1,17 +1,19 @@
 import { useState } from "react";
+
 const Search = ({ onSearch }) => {
   const [query, setQuery] = useState("");
 
   const handleChange = (event) => {
-    setQuery(event.target.value);
-    onSearch(event.target.value);
+    const inputValue = event.target.value;
+    setQuery(inputValue);
+    onSearch(inputValue);
   };
+
   return (
     <div>
-      <input type="text" value={query} onChange={handleChange}>
-        {onSearch}
-      </input>
+      <input type="text" value={query} onChange={handleChange} />
     </div>
   );
 };
+
 export default Search;
--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,21 +1,38 @@
 import { useState } from "react";
 import "./App.css";
 import Search from "../components/Search";
-
+import axios from "axios";
 function App() {
   const [countries, setCountries] = useState([]);
   const handleSearch = (query) => {
     if (query) {
       axios
-        .get("https://studies.cs.helsinki.fi/restcountries/api/name")
+        .get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
         .then((response) => {
-          setCountries(response.data);
+          console.log(response.data); // Check the response data
+          const filteredCountries = response.data.filter((country) =>
+            country.name.common.toLowerCase().startsWith(query.toLowerCase())
+          );
+
+          setCountries(filteredCountries);
+        })
+        .catch((error) => {
+          console.error("Error fetching countries:", error);
+          setCountries([]);
         });
+    } else {
+      setCountries([]);
     }
   };
+
   return (
     <div>
       <Search onSearch={handleSearch} />
+      <ul>
+        {countries.map((country) => (
+          <li key={country.cca3}>{country.name.common}</li>
+        ))}
+      </ul>
     </div>
   );
 }
