7e878ccb051583894779329e50cbf21bd07c3dd2
Abhishek khati chhetri <abhichhetri@outlook.com>
2024-06-24 21:34:48 +0300

    3.3 done. displayed person with specific id

--- part3/index.js
+++ part3/index.js
@@ -34,6 +34,15 @@ app.get("/info", (request, response) => {
   response.send(`<p>Phonebook has info for ${numberOfEntries} people</p>
     <p>${requestTime}</p>`);
 });
+app.get(`/api/persons/:id`, (request, response) => {
+  const id = request.params.id;
+  const person = phoneBook.find((person) => person.id === id);
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
