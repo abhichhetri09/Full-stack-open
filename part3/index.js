c796038f67f8ff0c6c8f043412edf3a0bd2c7770
Abhishek khati chhetri <abhichhetri@outlook.com>
2024-06-26 00:28:09 +0300

    morgan middleware added to log the custom message

--- part3/index.js
+++ part3/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-
+app.use(express.json());
 let phoneBook = [
   {
     id: "1",
@@ -48,6 +48,27 @@ app.delete("/api/persons/:id", (request, response) => {
   phoneBook = phoneBook.filter((person) => person.id !== id);
 
   response.status(204).end();
+});
+
+app.post("/api/persons", (request, require) => {
+  const body = request.body;
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "Name or number is missing",
+    });
+  }
+  if (phoneBook.find((person) => person.name === body.name)) {
+    return response.status(400).json({
+      error: "Name must be unique",
+    });
+  }
+  const newPerson = {
+    id: Math.floor(Math.random() * 10000).toString,
+    name: body.name,
+    number: body.number,
+  };
+  phoneBook.push(newPerson);
+  response.json(newPerson);
 });
 const PORT = 3001;
 app.listen(PORT, () => {
